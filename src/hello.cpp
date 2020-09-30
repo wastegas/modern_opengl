2b8fcd353c8da56f3d8e1c5260149041a8ea7bff
wastegas <wastegas@gmail.com>
2020-09-30 11:02:40 -1000

    add glClear

--- src/hello.cpp
+++ src/hello.cpp
@@ -34,6 +34,9 @@ int main()
 
   while(!glfwWindowShouldClose(window))
     {
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+      
       processInput(window);
       
       glfwSwapBuffers(window);
