4ce00a0c97e65eadc82e9d3c43a107a926ff7412
wastegas <wastegas@gmail.com>
2020-10-04 15:50:45 -1000

    new header source

---
+++ include/shader.h
@@ -0,0 +1,27 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader
+{
+ public:
+  // the program ID
+  unsigned int ID;
+
+  // constructore
+  Shader(const char* vertexPath, const char* fragmentPath);
+  // use/activate the shader
+  void use();
+  // utility uniform functions
+  void setBool(const std::string &name, bool value) const;
+  void setInt(const std::string &name, int value) const;
+  void setFloat(const std::string &name, float value) const;
+};
+
+#endif
