a27a63b34d036c58b4a65b9701c5d161165f2254
wastegas <wastegas@gmail.com>
2020-10-21 10:43:46 -1000

    add setMat4 function

--- include/shader.h
+++ include/shader.h
@@ -148,6 +148,11 @@ class Shader
     glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE,
 		       &mat[0][0]);
   }
+  void setMat4(const std::string &name, const glm::mat4 &mat) const
+  {
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE,
+		       &mat[0][0]);
+  }
 };
 
 #endif
