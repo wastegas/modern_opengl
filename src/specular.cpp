66b29ff2c9f319882380063a95d35b889840a46a
wastegas <wastegas@gmail.com>
2020-10-23 19:42:45 -1000

    add viewPos

--- src/specular.cpp
+++ src/specular.cpp
@@ -172,6 +172,7 @@ int main()
       glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
 		    (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
       glm::mat4 view = camera.GetViewMatrix();
+      lightingShader.setVec3("viewPos", camera.Position);
       lightingShader.setMat4("projection", projection);
       lightingShader.setMat4("view", view);
 
