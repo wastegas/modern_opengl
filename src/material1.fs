7810252ad317c7d70ee3ea2d310495b61cb7a385
wastegas <wastegas@gmail.com>
2020-10-24 19:07:40 -1000

    use material struct

--- src/material1.fs
+++ src/material1.fs
@@ -1,9 +1,17 @@
 #version 330 core
 out vec4 FragColor;
 
+struct Material {
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+  float shininess;
+};
+
 in vec3 Normal;
 in vec3 FragPos;
 
+uniform Material material;
 uniform vec3 viewPos;
 uniform vec3 lightPos;
 uniform vec3 objectColor;
@@ -12,21 +20,20 @@ uniform vec3 lightColor;
 void main()
 {
   // ambient
-  float ambientStrength = 0.1;
-  vec3 ambient = ambientStrength * lightColor;
+  vec3 ambient = material.ambient * lightColor;
 
   // diffuse
   vec3 norm = normalize(Normal);
   vec3 lightDir = normalize(lightPos - FragPos);
   float diff = max(dot(norm, lightDir), 0.0);
-  vec3 diffuse = diff * lightColor;
+  vec3 diffuse = lightColor * (diff * material.diffuse);
 
   // specular
-  float specularStrength = 0.5;
   vec3 viewDir = normalize(viewPos - FragPos);
   vec3 reflectDir = reflect(-lightDir, norm);
-  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-  vec3 specular = specularStrength * spec * lightColor;
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0),
+                           material.shininess);
+  vec3 specular = lightColor * (spec * material.specular);
 
   vec3 result = (ambient + diffuse + specular)  * objectColor;
   FragColor = vec4(result, 1.0);
