b98bdd9d6f17b78b6a779fe5cf81c62510eda037
wastegas <wastegas@gmail.com>
2020-10-24 19:33:54 -1000

    add light struct

--- src/material2.fs
+++ src/material2.fs
@@ -8,10 +8,20 @@ struct Material {
   float shininess;
 };
 
+struct Light {
+  vec3 position;
+
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+};
+
 in vec3 Normal;
 in vec3 FragPos;
 
 uniform Material material;
+uniform Light    light;
+
 uniform vec3 viewPos;
 uniform vec3 lightPos;
 uniform vec3 objectColor;
@@ -20,20 +30,20 @@ uniform vec3 lightColor;
 void main()
 {
   // ambient
-  vec3 ambient = material.ambient * lightColor;
+  vec3 ambient = material.ambient * light.ambient;
 
   // diffuse
   vec3 norm = normalize(Normal);
   vec3 lightDir = normalize(lightPos - FragPos);
   float diff = max(dot(norm, lightDir), 0.0);
-  vec3 diffuse = lightColor * (diff * material.diffuse);
+  vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
   // specular
   vec3 viewDir = normalize(viewPos - FragPos);
   vec3 reflectDir = reflect(-lightDir, norm);
   float spec = pow(max(dot(viewDir, reflectDir), 0.0),
                            material.shininess);
-  vec3 specular = lightColor * (spec * material.specular);
+  vec3 specular = light.specular * (spec * material.specular);
 
   vec3 result = (ambient + diffuse + specular)  * objectColor;
   FragColor = vec4(result, 1.0);
