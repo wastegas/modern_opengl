dcabc4a25df3ca0b1a5db8a63c83f51e203ae4d0
wastegas <wastegas@gmail.com>
2020-10-24 19:05:49 -1000

    user material properties for shader attributes

--- src/material1.cpp
+++ src/material1.cpp
@@ -168,6 +168,12 @@ int main()
       lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
       lightingShader.setVec3("lightPos", lightPos);
 
+      // set our material values"
+      lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+      lightingShader.setVec3("material.diffuse", 1.0f, 1.0f, 1.0f);
+      lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+      lightingShader.setFloat("material.shininess", 32.0f);
+
       // view projection transformations
       glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
 		    (GLfloat)SCR_WIDTH / (GLfloat)SCR_HEIGHT, 0.1f, 100.0f);
