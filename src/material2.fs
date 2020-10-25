c3bb31dadbbea9a340079bc0bd7c82027c4939fb
wastegas <wastegas@gmail.com>
2020-10-24 19:21:17 -1000

    shaders for material2

---
+++ src/material2.fs
@@ -0,0 +1,40 @@
+#version 330 core
+out vec4 FragColor;
+
+struct Material {
+  vec3 ambient;
+  vec3 diffuse;
+  vec3 specular;
+  float shininess;
+};
+
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform Material material;
+uniform vec3 viewPos;
+uniform vec3 lightPos;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+
+void main()
+{
+  // ambient
+  vec3 ambient = material.ambient * lightColor;
+
+  // diffuse
+  vec3 norm = normalize(Normal);
+  vec3 lightDir = normalize(lightPos - FragPos);
+  float diff = max(dot(norm, lightDir), 0.0);
+  vec3 diffuse = lightColor * (diff * material.diffuse);
+
+  // specular
+  vec3 viewDir = normalize(viewPos - FragPos);
+  vec3 reflectDir = reflect(-lightDir, norm);
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0),
+                           material.shininess);
+  vec3 specular = lightColor * (spec * material.specular);
+
+  vec3 result = (ambient + diffuse + specular)  * objectColor;
+  FragColor = vec4(result, 1.0);
+}
