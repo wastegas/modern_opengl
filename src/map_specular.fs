ffa386020233ce9c7680cd04bf37bced52f7e1b0
wastegas <wastegas@gmail.com>
2020-10-26 19:42:37 -1000

    fix specular computation

--- src/map_specular.fs
+++ src/map_specular.fs
@@ -3,7 +3,7 @@ out vec4 FragColor;
 
 struct Material {
   sampler2D diffuse;
-  vec3 specular;
+  sampler2D specular;
   float shininess;
 };
 
@@ -42,7 +42,8 @@ void main()
   vec3 reflectDir = reflect(-lightDir, norm);
   float spec = pow(max(dot(viewDir, reflectDir), 0.0),
                            material.shininess);
-  vec3 specular = light.specular * (spec * material.specular);
+  vec3 specular = light.specular * spec * texture(material.specular,
+                                              TexCoords).rgb;
 
   vec3 result = ambient + diffuse + specular;
   FragColor = vec4(result, 1.0);
