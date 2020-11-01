ee3d25bdc080447e70e694d4449d64f8ffe008b0
wastegas <wastegas@gmail.com>
2020-11-01 08:36:41 -1000

    change color clear bit

--- src/modelLoading.cpp
+++ src/modelLoading.cpp
@@ -79,7 +79,7 @@ int main()
       lastFrame = currentFrame;
 
       
-      glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+      glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
       ourShader.use();
