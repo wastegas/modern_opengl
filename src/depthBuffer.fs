82ee209544230ca6f9bb40e222fc231436be5ceb
wastegas <wastegas@gmail.com>
2020-11-04 08:38:54 -1000

    linearize depth

--- src/depthBuffer.fs
+++ src/depthBuffer.fs
@@ -5,7 +5,17 @@ in vec2 TexCoords;
 
 uniform sampler2D texture1;
 
+float near = 0.1;
+float far = 100.0;
+
+float LinearizeDepth(float depth)
+{
+  float z = depth * 2.0 - 1.0; // back
+  return (2.0 * near * far) / (far + near -z * (far - near));
+}
+
 void main()
 {
-  FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
+  float depth = LinearizeDepth(gl_FragCoord.z) / far;
+  FragColor = vec4(vec3(depth), 1.0);
 }
