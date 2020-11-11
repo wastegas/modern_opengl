376252f35d39213a14f9b931df06bec39f2126fe
wastegas <wastegas@gmail.com>
2020-11-11 08:37:28 -1000

    create a weighted average

--- src/framebufferScreenPost.fs
+++ src/framebufferScreenPost.fs
@@ -8,6 +8,8 @@ uniform sampler2D screenTexture;
 void main()
 {
   FragColor = texture(screenTexture, TexCoords);
-  float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+  float average = 0.2126 * FragColor.r +
+                  0.7152 * FragColor.g +
+		  0.0772 * FragColor.b;
   FragColor = vec4(average, average, average, 1.0);
 }
