22d6a48f4cec2b8fc0253c33746d7745c5b1e91b
wastegas <wastegas@gmail.com>
2020-12-13 10:25:57 -1000

    source for geometryNormals

---
+++ src/geometryNormals.cpp
@@ -0,0 +1,176 @@
+/* demonstrate model loading */
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
+
+#include <shader.h>
+#include <camera.h>
+#include <model.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, GLint width, GLint height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, GLdouble xpos, GLdouble ypos);
+void scroll_callback(GLFWwindow* window,GLdouble xoffset, GLdouble yoffset);
+
+
+// screen size
+const GLuint SCR_WIDTH = 1024;
+const GLint SCR_HEIGHT = 768;
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+GLboolean firstMouse = true;
+GLfloat lastX = SCR_WIDTH / 2.0; // half of width of window
+GLfloat lastY = SCR_HEIGHT / 2.0; // half of height of window
+
+// frame delta time
+GLfloat deltaTime = 0.0f; // Time between current frame and last frame
+GLfloat lastFrame = 0.0f; // Time of last frame
+
+
+int main()
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,
+					"LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+    {
+      std::cout << "Failed to create GLFW window" << std::endl;
+      glfwTerminate();
+      return -1;
+    }
+
+  glfwMakeContextCurrent(window);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
+
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+      std::cout << "Failed to initialize GLAD" << std::endl;
+      return -1;
+    }
+
+  // configure global opengl state
+  glEnable(GL_DEPTH_TEST);
+
+  
+  Shader shader("./default.vs", "default.fs", nullptr);
+  Shader normalShader("./geometryNormals.vs", "./geometryNormals.fs",
+		      "./geometryNormals.gs");
+  
+  Model backpack("./backpack.obj");
+
+  
+  while(!glfwWindowShouldClose(window))
+    {
+      //per-frame time logic
+      GLfloat currentFrame = glfwGetTime();
+      deltaTime = currentFrame - lastFrame;
+      lastFrame = currentFrame;
+
+      
+      glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+      // view/projection transformations
+      glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
+	      (GLfloat)SCR_WIDTH / (GLfloat) SCR_HEIGHT, 1.0f, 100.0f);
+      glm::mat4 view = camera.GetViewMatrix();
+      glm::mat4 model = glm::mat4(1.0f);
+      shader.use();
+      shader.setMat4("projection", projection);
+      shader.setMat4("view", view);
+      shader.setMat4("model", model);
+
+      backpack.Draw(shader);
+
+      // draw with normal visualizing geometry shader
+      normalShader.use();
+      normalShader.setMat4("projection", projection);
+      normalShader.setMat4("view", view);
+      normalShader.setMat4("model", model);
+		      
+      backpack.Draw(normalShader);
+      
+      processInput(window);
+      
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    }
+
+
+  // terminate clearing all previously allocated GLFW resources
+  glfwTerminate();
+  
+  
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, GLint width, GLint height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+  const GLfloat cameraSpeed = 2.5f * deltaTime;
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+      glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+      camera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+      camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+      camera.ProcessKeyboard(LEFT, deltaTime);
+    }
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+      camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+				  
+}
+
+// glfw: whenever the mouse mves, this callback is called
+void mouse_callback(GLFWwindow* window, GLdouble xpos, GLdouble ypos)
+{
+  if (firstMouse)
+    {
+      lastX = xpos;
+      lastY = ypos;
+      firstMouse = false;
+    }
+
+  GLfloat xoffset = xpos - lastX;
+  GLfloat yoffset = lastY - ypos; // reversed since y-coord go from bottom to top
+  lastX = xpos;
+  lastY = ypos;
+
+  camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, GLdouble xoffset, GLdouble yoffset)
+{
+  camera.ProcessMouseScroll(yoffset);
+}
+
